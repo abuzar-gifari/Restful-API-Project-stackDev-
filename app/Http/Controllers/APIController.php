c9a32b93868fabf3f5286a2f5821ade16fe8571c
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2022-05-05 22:46:11 +0600

    api authentication done without passport

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator as FacadesValidator;
-use Validator;
 
 class APIController extends Controller
 {
@@ -145,5 +146,93 @@ class APIController extends Controller
     public function DeleteUser($id){
         User::where('id',$id)->delete();
         return response()->json([ "message"=>"user deleted successfully" ],202);    
+    }
+
+    public function getUsersList(Request $request){
+        $header=$request->header('Authorization');
+        if (empty($header)) {
+            $message="Header Authorization part is missing";
+            return response()->json([ "message"=>$message ],422);
+        }else {
+            if ($header == "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkFidXphciBHaWZhcmkiLCJpYXQiOjE1MTYyMzkwMjJ9.r4vCuN7pdA0vC6YMUokA7C6742h8s_zn2SiXCggdeac") {
+                $users=User::get();
+                return response()->json([ "users"=>$users ],202);    
+            }else {
+                $message="Header Authorization part is incorrect";
+                return response()->json([ "status"=>false,"message"=>$message ],422);
+            }
+                
+        }
+    }
+
+    public function RegisterUser(Request $request){
+        if ($request->isMethod('post')) {
+            $userData = $request->input();
+            $apiToken = Str::random(60);
+
+            $user = new User();
+            $user->name = $userData['name'];
+            $user->email = $userData['email'];
+            $user->password = bcrypt($userData['password']);
+            $user->api_token = $apiToken;
+            $user->save();
+            return response()->json([
+                "status"=>true, 
+                "message"=>"user registered successfully",
+                "token"=>$apiToken 
+            ],201);
+        }
+    }
+
+    public function LoginUser(Request $request){
+        if ($request->isMethod('post')) {
+            $userData = $request->input();
+            $apiToken = Str::random(60);
+
+            // ADVANCE VALIDATION PROCESS   
+            $rules=[
+                "email"=>"required|email|exists:users",
+                "password"=>"required",
+            ];
+
+            $customMessages=[
+                "name.required"=>"your name is required",
+                "email.required"=>"your email is required",
+                "email.email"=>"your email is not valid",
+                "email.exists"=>"email already exists",
+                "password.required"=>"password is required",
+            ];
+
+            $validator = FacadesValidator::make($userData,$rules,$customMessages);
+
+            if ($validator->fails()) {
+                return response()->json([ "success"=>false,"message"=>$validator->errors() ],422);    
+            }
+
+            $userDetails=User::where('email',$userData['email'])->first();
+
+            if (password_verify($userData['password'],$userDetails->password)) {
+                $apiToken=Str::random(60);
+                User::where('email',$userData['email'])->update(['api_token'=>$apiToken],200);
+                return response()->json([ "success"=>true,"message"=>"User Login Success" ],200);
+            }else {
+                return response()->json([ "success"=>false,"message"=>"password is incorrect" ],422);    
+            }
+        }
+    }
+
+    public function LogoutUser(Request $request){
+        $api_token=$request->header('Authorization');
+        if (empty($api_token)) {
+            $message="User token is missing in api header";
+            return response()->json([ "message"=>$message ],422);
+        }else {
+            $api_token = str_replace("Bearer ","",$api_token);
+            $userCount = User::where('api_token',$api_token)->count();
+            if ($userCount>0) {
+                User::where('api_token',$api_token)->update(['api_token'=>null]);
+                return response()->json([ "success"=>true,"message"=>"User Logout Successfully" ],200);
+            }              
+        }        
     }
 } 
--- routes/api.php
+++ routes/api.php
@@ -10,8 +10,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // custom routes
 Route::get('/users/{id?}',[APIController::class,'getUsers']);
+Route::get('/users-list',[APIController::class,'getUsersList']);
 Route::post('/add-users',[APIController::class,'addUsers']);
 Route::post('/add-multiple-users',[APIController::class,'addMultipleUsers']);
 Route::put('/update-user-details/{id?}',[APIController::class,'UpdateUserDetails']);
 Route::patch('/update-user-name/{id}',[APIController::class,'UpdateUserName']);
 Route::delete('/delete-user/{id}',[APIController::class,'DeleteUser']);
+
+Route::post('register-user',[APIController::class,'RegisterUser']);
+Route::post('login-user',[APIController::class,'LoginUser']);
+Route::post('logout-user',[APIController::class,'LogoutUser']);
